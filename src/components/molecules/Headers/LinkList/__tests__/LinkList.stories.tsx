cb9401ce45256a1ad9a973e3a9bc662b6c776875
Kontam <tehayan0111@gmail.com>
2021-06-09 17:59:18 +0900

    env: add storybook

---
+++ src/components/molecules/Headers/LinkList/__tests__/LinkList.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+
+import Component, { Props } from '../LinkList';
+
+export default {
+  title: 'molecules/Headers/LinkList',
+} as Meta;
+
+const Template: Story<Props> = (args) => <Component {...args} />;
+
+const data = [
+  { id: 1, name: 'Boost your conversion rate', href: '#' },
+  {
+    id: 2,
+    name: 'How to use search engine optimization to drive traffic to your site',
+    href: '#',
+  },
+  { id: 3, name: 'Improve your customer experience', href: '#' },
+];
+
+export const Default = Template.bind({});
+Default.args = { links: data };
