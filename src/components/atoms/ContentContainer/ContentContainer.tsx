a527d12dbe3fd3c3db1a35262d695c9bd540dcea
Kontam <tehayan0111@gmail.com>
2021-06-13 00:42:12 +0900

    feat: ssr graph style

--- src/components/atoms/ContentContainer/ContentContainer.tsx
+++ src/components/atoms/ContentContainer/ContentContainer.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const ContentContainer: React.FC = (props) => {
-  return <div className="w-full flex justify-center">{props.children}</div>;
+  return (
+    <div className="w-full min-h-screen flex justify-center">
+      {props.children}
+    </div>
+  );
 };
 
 export default ContentContainer;
--- src/components/molecules/Graph/index.tsx
+++ src/components/molecules/Graph/index.tsx
@@ -5,7 +5,7 @@ import { useLineChart } from './useLineChart';
 const Graph: React.FC = () => {
   const chartProps = useLineChart();
   return (
-    <div className="max-w-80p">
+    <div className="max-w-80p w-10/12">
       <Line type="line" data={chartProps.data} />
     </div>
   );
